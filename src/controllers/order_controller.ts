8a9edeac691f4756cd8c0a791227fcbeeeb7e5ea
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-02-08 01:34:16 +0100

    Cleaning up code

--- src/controllers/order_controller.ts
+++ src/controllers/order_controller.ts
@@ -93,6 +93,7 @@ export const store = async (req: Request, res: Response) => {
             });
             }
         }
+        
 
         const postOrders = await prisma.order.create({
             data: {
